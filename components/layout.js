e557ffa8c37328553fb72e575474c909e6bd71c7
wuhelong <5387348+wuhelong@user.noreply.gitee.com>
2024-12-18 10:49:25 +0800

    costom nextjs

---
+++ components/layout.js
@@ -0,0 +1,59 @@
+import Head from 'next/head';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Your Name';
+export const siteTitle = 'Next.js Sample Website';
+
+export default function Layout({ children, home = false }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Learn how to build a personal website using Next.js" />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/user.png"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}:home is true</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <img
+                src="/images/user.png"
+                className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                alt={name}
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}home is false
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+  return <h1>404-page not Found --from Custom404</h1>;
+}
